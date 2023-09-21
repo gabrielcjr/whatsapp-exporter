30c026fa0e9b6b0e013035b2eb286a1340debf16
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2023-09-21 13:22:59 -0300

    exporting fields correctly

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from read_file import turn_file_into_list
+# from read_file import turn_file_into_list
 import time, csv
 from bs4 import BeautifulSoup
+import re
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://web.whatsapp.com/")
-
-# users = turn_file_into_list('r')
 
 wait = WebDriverWait(driver, 600)
 
@@ -49,31 +47,49 @@ def scroll_screen_to_top_and_copy():
     
     # text = driver.find_element(By.XPATH, value=x_path)
 
-    text = driver.find_elements(By.CLASS_NAME, "_1DETJ copyable-text")
-
-    
-
-    all_chats = []
-
-    for item in text:
-        soup = BeautifulSoup(item)
-        soup.
-        data_path = "//div[@role='application']"
-        date_and_phone = 
-
-
+    text = element.get_attribute("outerHTML")
     return text
 
-result = scroll_screen_to_top_and_copy()
+html_chat = scroll_screen_to_top_and_copy()
 
 def process_web_element(webelement):
+    soup = BeautifulSoup(webelement, "html.parser")
 
+    elements_with_attribute = soup.select("._1DETJ")
 
+    complete_messages = []
 
-file_path = "output.txt"
+    for element in elements_with_attribute:
+        single_message = []
+        data_pre_plain_text = element['data-pre-plain-text']
+        single_message.append(data_pre_plain_text)
+        message = element.select(".selectable-text")
+        message = BeautifulSoup(message[0].span.prettify(), "html.parser")
+        single_message.append(message.get_text())
+        complete_messages.append(single_message)
 
-# Open the file in write mode ('w')
-with open(file_path, 'w', encoding="utf_8") as file:
-    # Write the text to the file
-    file.write(result)
+    return complete_messages
+
+messages_list = process_web_element(html_chat)
+
+def export_to_csv(list):
+    output_data = []
+
+    for entry in list:
+        entry_str = entry[0] + entry[1] 
+        match = re.search(r'\[(\d{2}:\d{2}), (\d{2}/\d{2}/\d{4})\] (\+\d+ \d+ \d{4,5}-\d{4}): (.*)', entry_str)
+        if match:
+            time, date, phone_number, message = match.groups()
+            output_data.append([phone_number, date, time, message])
+
+    csv_filename = "output.csv"
+
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        writer.writerow(["Phone Number", "Date", "Time", "Message"])
+        
+        writer.writerows(output_data)
+
+    print(f'Data exported to "{csv_filename}"')
 
